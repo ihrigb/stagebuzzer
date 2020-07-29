0819ddc47571fd247f1941c91d9afa7dd205cf2b
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-07-29 11:05:31 +0200

    Fixes

--- controls/_controls.py
+++ controls/_controls.py
@@ -4,7 +4,7 @@ from display import Display
 
 class Controls:
     def __init__(self, display: Display):
-        self._up_button = Button(31, lambda: display.up())
-        self._left_button = Button(33, lambda: display.esc())
-        self._down_button = Button(35, lambda: display.down())
-        self._right_button = Button(36, lambda: display.ok())
+        self._up_button = Button(31, lambda ch: display.up())
+        self._left_button = Button(33, lambda ch: display.esc())
+        self._down_button = Button(35, lambda ch: display.down())
+        self._right_button = Button(36, lambda ch: display.ok())
