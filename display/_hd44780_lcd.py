7b159cda82a1d40c57feb37d531a19bf06176a1a
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-07-29 12:07:19 +0200

    Fixes

--- display/_hd44780_lcd.py
+++ display/_hd44780_lcd.py
@@ -2,8 +2,11 @@ from ._lcd import Lcd
 import RPi.GPIO as GPIO
 from RPLCD import CharLCD
 
-GPIO.setmode(GPIO.BOARD)
-lcd = CharLCD(pin_rs=26, pin_rw=7, pin_e=24, pins_data=[16, 12, 10, 8], numbering_mode=GPIO.BOARD, cols=20, rows=4,
+#GPIO.setmode(GPIO.BOARD)
+#lcd = CharLCD(pin_rs=26, pin_rw=7, pin_e=24, pins_data=[16, 12, 10, 8], numbering_mode=GPIO.BOARD, cols=20, rows=4,
+#              dotsize=8)
+GPIO.setmode(GPIO.BCM)
+lcd = CharLCD(pin_rs=7, pin_rw=4, pin_3=8, pins_data=[23, 18, 15, 14], numbering_mode=GPIO.BCM, cols=20, rows=4,
               dotsize=8)
 
 
