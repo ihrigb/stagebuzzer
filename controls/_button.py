ca9e3dbec32f6ddec8e61f8780bf155c09ad3d03
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-07-29 12:33:20 +0200

    Fixes

--- controls/_button.py
+++ controls/_button.py
@@ -3,11 +3,11 @@ import RPi.GPIO as GPIO
 
 class Button:
     def __init__(self, name: str, gpio_pin: int, on_push):
-
+        self._name = name
         self._on_push = on_push
 
         GPIO.setup(gpio_pin, GPIO.IN)
-        GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=self._on_push, bouncetime=200)
+        GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=self.on_push, bouncetime=200)
 
     def on_push(self):
         print("Button {} pushed.".format(self._name))
