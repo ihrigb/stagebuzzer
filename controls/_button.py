2f9276a13c6c9f2561164005ca507b58dcd42bb0
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-08-06 10:51:15 +0200

    Stuff

--- controls/_button.py
+++ controls/_button.py
@@ -7,8 +7,8 @@ class Button:
         self._name = name
         self._on_push = on_push
 
-        GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=self.on_push, bouncetime=200)
+        GPIO.setup(gpio_pin, GPIO.IN)
+        GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=self.on_push, bouncetime=500)
 
     def on_push(self, channel):
         print("Button {} pushed.".format(self._name))
