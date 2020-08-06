553d660f5b0c4e2f49774d166a7ebd795b0f8ef8
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-08-06 09:55:34 +0200

    Stuff

--- controls/_button.py
+++ controls/_button.py
@@ -1,12 +1,13 @@
 import RPi.GPIO as GPIO
+from typing import Callable
 
 
 class Button:
-    def __init__(self, name: str, gpio_pin: int, on_push):
+    def __init__(self, name: str, gpio_pin: int, on_push: Callable[[int], None]):
         self._name = name
         self._on_push = on_push
 
-        GPIO.setup(gpio_pin, GPIO.IN)
+        GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=self.on_push, bouncetime=200)
 
     def on_push(self, channel):
