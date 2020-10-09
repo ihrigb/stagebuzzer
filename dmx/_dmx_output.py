909064381144535fdf98856a85f5fc11d4a3535f
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-10-09 16:26:29 +0200

    Stuff

--- dmx/_dmx_output.py
+++ dmx/_dmx_output.py
@@ -1,11 +1,12 @@
 from ._dmx import Dmx
 
-dmx_channels = 512
-
 
 class DmxOutput:
 
-    _dmx = Dmx('/dev/ttyUSB0')
+    def __init__(self):
+        self._dmx = Dmx('/dev/ttyUSB0')
+        self._dmx.blackout()
+        self._dmx.render()
 
     def set(self, channel: int, value: int):
         self._dmx.set_channel(channel, value)
