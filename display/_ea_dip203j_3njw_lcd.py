a78b5eaa849b90ffe0d48efd15b8d34d897fb59a
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-10-19 13:00:50 +0200

    Stuff

--- display/_ea_dip203j_3njw_lcd.py
+++ display/_ea_dip203j_3njw_lcd.py
@@ -6,7 +6,7 @@ from RPLCD import CharLCD
 # lcd = CharLCD(pin_rs=26, pin_rw=7, pin_e=24, pins_data=[16, 12, 10, 8], numbering_mode=GPIO.BOARD, cols=20, rows=4,
 #              dotsize=8)
 GPIO.setmode(GPIO.BCM)
-lcd = CharLCD(pin_rs=7, pin_rw=4, pin_e=8, pins_data=[23, 18, 15, 14], numbering_mode=GPIO.BCM, cols=20, rows=4,
+lcd = CharLCD(pin_rs=18, pin_rw=23, pin_e=24, pins_data=[25, 8, 7, 12], numbering_mode=GPIO.BCM, cols=20, rows=4,
               dotsize=8)
 
 
