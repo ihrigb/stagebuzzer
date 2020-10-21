80cbaaa51c6a96786c6eb8e506de27e2fe8e4792
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-10-21 14:20:18 +0200

    Stuff

--- dmx/_dmx_buzzer_callback.py
+++ dmx/_dmx_buzzer_callback.py
@@ -1,5 +1,6 @@
 import buzzer
 import config
+import logging
 from ._dmx_output import DmxOutput
 
 
@@ -10,9 +11,11 @@ class DmxBuzzerCallback(buzzer.BuzzerCallback):
         self._dmx_config = dmx_config
 
     def on_buzz(self, buz: str):
+        logging.debug('DMX callback called.')
         address = self._dmx_config.get_dmx_address(buz)
         value = self._dmx_config.get_dmx_value(buz)
         self._output.set(address, value)
 
     def reset(self):
+        logging.debug('DMX reset called.')
         self._output.reset()
