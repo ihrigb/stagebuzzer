03711fc8261237d179bd226356a0790cfb632014
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2025-05-25 21:48:59 +0200

    Turn buttons

--- controls/_controls.py
+++ controls/_controls.py
@@ -1,14 +1,14 @@
 from ._button import Button
 from display import Display, ButtonLights
 
-_up_button_pin = 5
-_left_button_pin = 27
-_down_button_pin = 9
-_right_button_pin = 17
-_up_led_pin = 11
-_left_led_pin = 22
-_down_led_pin = 10
-_right_led_pin = 6
+_up_button_pin = 9
+_left_button_pin = 17
+_down_button_pin = 5
+_right_button_pin = 27
+_up_led_pin = 10
+_left_led_pin = 6
+_down_led_pin = 11
+_right_led_pin = 22
 
 
 class Controls:
